d11b097d206e140e6d9c66027c9ed96c591f0057
Md Al Amin <fpalamin6@gmail.com>
2023-02-28 23:59:20 +0600

    route added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Main from './Layout/Main/Main';
+import LogIn from './Pages/LogIn/LogIn';
+import SingUp from './Pages/SingUp/SingUp';
 
 function App() {
+  const routes = createBrowserRouter([
+    {
+      path: '/', element: <Main></Main>,
+      children:[
+        // { path: '/', element: <Home></Home> },
+        { path: '/login', element: <LogIn></LogIn> },
+        { path: '/singup', element: <SingUp></SingUp> },
+      ]
+    }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={routes}></RouterProvider>
     </div>
   );
 }
